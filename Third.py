269b576428c4b5ef4b59eafb74a1114d576b4470
Sai <vaidyasaiteja143@gmail.com>
2026-09-16 15:05:57 +0700

    Hello new

---
+++ Third.py
@@ -0,0 +1,13 @@
+i want to create a thirs 
+
+Third file is for the third task of the first week
+
+Fourth file is for the fourth task of the first week
+
+Fifth file is for the fifth task of the first week
+
+Sixth file is for the sixth task of the first week
+
+Seventh file is for the seventh task of the first week
+
+Eighth file is for the eighth task of the first week        
